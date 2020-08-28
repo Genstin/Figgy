ab9d191723d5a138cc405452d58a1f51ffb8d344
Pigerly <contact@pigerly.com>
2020-08-28 15:05:43 -0400

    Added .fig Refrence & Basic Meathods

--- src/io/gofiggy/figgy/dotfig/DotFigFile.java
+++ src/io/gofiggy/figgy/dotfig/DotFigFile.java
@@ -1,5 +1,47 @@
 package io.gofiggy.figgy.dotfig;
 
-public class DotFigFile {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import io.gofiggy.figgy.dotfig.types.DotFigStage;
+
+public class DotFigFile {
+	DotFigStage defaultStage = new DotFigStage();
+	HashMap<DotFigStage, ArrayList<DotFigStage>> stages = new HashMap<DotFigStage, ArrayList<DotFigStage>>();
+	
+	public DotFigFile() {
+		
+	}
+	
+	public DotFigStage[] getStages() {
+		ArrayList<DotFigStage> returner = new ArrayList<DotFigStage>();
+		stages.forEach((parent, item) -> {
+			item.forEach((subitem) -> {
+				returner.add(subitem);
+			});
+		});
+		DotFigStage[] returner2 = (DotFigStage[]) returner.toArray();
+		return returner2;
+	}
+	
+	public DotFigStage getDefaultStage() {
+		return defaultStage;
+	}
+	
+	public void createFromContents() {
+		
+	}
+	
+	public void importFromContents() {
+	}
+	
+	public String exportContents() {
+		String builder = "";
+		
+		return null;
+	}
+	
+	public String toString() {
+		return exportContents();
+	}
 }
