ecc38fd1f707ce0a31c895a13062f30daaf1dd8d
Pigerly <contact@pigerly.com>
2020-08-29 22:00:12 -0400

    Major Changes

--- src/io/gofiggy/figgy/converter/refrences/JSONSyntax.java
+++ src/io/gofiggy/figgy/converter/refrences/JSONSyntax.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import io.gofiggy.figgy.internal.editor.Editor;
 import io.gofiggy.figgy.internal.editor.types.content.Content;
-import io.gofiggy.figgy.internal.utils.Converter;
 import io.gofiggy.figgy.tools.JsonTools;
 
 public class JSONSyntax {
@@ -43,51 +42,60 @@ public class JSONSyntax {
 			if((Math.floor(file.getContents().size() - 1)) == contMentor) builder = builder.substring(0, builder.length() - 1);
 			builder = builder + newlineSymbol;
 		}
-		
 		builder = builder + "}";
 		return builder;
 	}
 	
-	
-	protected String render_subobject_content(Content cont, Boolean prettyPrint) {
-		return null;
-	}
-	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	protected String render_hashmap_object(Content cont, String builder, String newlineSymbol, String tabSymbol, String tabBuilder, Boolean prettyPrint) {
+	protected String render_hashmap_object(Content cont, String builder, String newlineSymbol, String tabSymbol, String tabBuilder, Boolean prettyPrint, Integer spacers) {
 		builder = builder + "{" + newlineSymbol;
 		HashMap map = (HashMap) cont.getValue().getValue();
 		mapMentor = -1;
 		insertHelper = "";
 		Iterable map2 = null;
 		map.forEach((k, v) -> {
-			String tabSymbol2 = "   ";
-			String newlineSymbol2 = "\n";
-			if(prettyPrint == false) { newlineSymbol2 = ""; tabSymbol2 = "";
-			} else if(prettyPrint == null) { newlineSymbol2 = "%NEW_LINE%"; tabSymbol2 = ""; }
 			mapMentor = mapMentor + 1;
 			String subbuilder = "";
 			String subbuilder1 = "";
 			String subbuilder2 = "";
-			String tabBuilder2 = tabSymbol2 + tabSymbol2;
-			if((new Converter(k)).toString() != null) { subbuilder1 = "\"" + (new Converter(k)).toString() + "\": "; } else subbuilder1 = "\"" + (new Converter(k)).getValue() + "\": ";
-			if((new Converter(v)).toString() != null) {	subbuilder2 = "\"" + (new Converter(v)).toString() + "\""; } else subbuilder2 = "" + (new Converter(v)).getValue() + "";
-			subbuilder = tabBuilder2 + subbuilder1 + subbuilder2 + ",";
-			if((Math.floor(map.size() - 1)) == mapMentor) subbuilder = subbuilder.substring(0, subbuilder.length() - 1);
-			insertHelper = insertHelper + subbuilder + "" + newlineSymbol2;
+			String tabBuilder2 = tabSymbol + tabSymbol;
+			Content newContent = new Content();
+			newContent.setKey("" + k + "");
+			newContent.setValue(v);
+			insertHelper = insertHelper + render_subcontent(newContent, prettyPrint, (spacers + 1));
+			insertHelper = insertHelper + subbuilder + "" + newlineSymbol;
 		});
 		builder = builder + insertHelper + tabBuilder + "}";
 		return builder;
 	}
 	
+	protected String render_array_object(Content cont, String builder, String newlineSymbol, String tabSymbol, String tabBuilder, Boolean prettyPrint) {
+		builder = builder + "[" + newlineSymbol;
+		builder = builder + tabBuilder + "]";
+		return builder;
+	}
+	
+	protected String render_subcontent(Content cont, Boolean prettyPrint, Integer spacers) {
+		String gen = "";
+		if(cont.getKey().makeConverter().toString() != null) {
+			gen = cont.getKey().makeConverter().toString() + "\": ";
+		} else {
+			gen = "\"" + cont.getKey().makeConverter().getValue() + "\": ";
+		}
+		if(cont.getValue().toString() != null) {
+			gen = "\"" + cont.getValue().toString() + "\"";
+		} else {
+			gen = "" + cont.getValue().getValue() + "";
+		}
+		return gen;
+	}
+	
 	protected String render_subobject(Content cont, Boolean prettyPrint, Integer spacers) {
 		String newlineSymbol = "\n"; 
 		String tabSymbol = "   ";
-		
 		for(Integer i = 0; i <= spacers; i++) {
 			tabSymbol = tabSymbol + tabSymbol;
 		}
-		
 		if(prettyPrint == false) {
 			newlineSymbol = "";
 			tabSymbol = "";
@@ -95,17 +103,13 @@ public class JSONSyntax {
 			newlineSymbol = "%NEW_LINE%";
 			tabSymbol = "";
 		}
-		
 		String tabBuilder = "";
 		String builder = "";
-		
 		if(cont.getValue().getValueType().equals(HashMap.class)) {
-			builder = render_hashmap_object(cont, builder, newlineSymbol, tabSymbol, tabBuilder, prettyPrint);
+			builder = render_hashmap_object(cont, builder, newlineSymbol, tabSymbol, tabBuilder, prettyPrint, spacers);
 		} else {
-			builder = builder + "[" + newlineSymbol;
-			builder = builder + tabBuilder + "]";
+			builder = render_array_object(cont, builder, newlineSymbol, tabSymbol, tabBuilder, prettyPrint);
 		}
-		
 		return builder;
 	}
 	
@@ -117,8 +121,6 @@ public class JSONSyntax {
 	}
 	
 	public Editor toDotFig(JsonTools editor) {
-		
 		return null;
-		
 	}
 }
